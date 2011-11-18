56be47da4e5c1623c345f462966c9536ebad4318
aj <aj@malton.name>
2011-11-18 12:53:02 +0000

    typos; features of divinum-get testings; moved directory

--- admin/divinum-get.pl
+++ admin/divinum-get.pl
@@ -3,13 +3,15 @@ use warnings;
 use strict;
 use Getopt::Long;
 use Date::Format;
+use URL::Encode ('url_encode');
 use Date::Calc
     qw/check_date Date_to_Days Add_Delta_Days Decode_Date_US Today/;
 
 my ($y, $m, $d) = Today();
 
-my @horae = qw/Matutinum Laudes Prima Tertia Sexta Nona Vespera Completorium SanctaMissa/;
-my $horae_list = join(' ', @horae);
+my @prayers = qw/Matutinum Laudes Prima Tertia Sexta Nona Vespera Completorium SanctaMissa/;
+my $prayers = join(' ', @prayers);
+my %prayers = map (($_,$_), @prayers);
 
 my @versions = (
     'Trident 1570',
@@ -18,86 +20,182 @@ my @versions = (
     'Reduced 1955',
     'Rubrics 1960'
 );
-my $version_list = join('|', @versions);
+my $versions = join('|', @versions);
+
+my @actions = qw/pray kalendar edit popup/;
+my $actions = join(' ', @actions);
+my %actions = map (($_,$_), @actions);
 
 my @params = qw/accented browsertime caller expand expandnum lang1 lang2 local
                 notes priest screenheight searchvalue setup testmode votive/;
-my $params_list = join(' ', @params);
+my $params = join(' ', @params);
+my %params = map (($_,$_), @params);
+
+my $prayer;
+my $action = 'pray';
+my $compare;
+my $mobile;
+my $version;
+my $from = "$m-$d-$y";
+my $to;
+
+my $query;
+my @cgi = ();
+
+my $entry;
+my $dir;
+my $base_url = 'http://divinumofficium.com/cgi-bin';
+sub resource_to_filename($);
+
+my $help;
 
 my $USAGE = <<USAGE ;
 Establish divinumofficium web results for a given hour and a range of dates.
-Usage: divinum-get [--hora=HORA|--missa] [option...]
+Usage: divinum-get [--prayer=PRAYER|--query=QUERY] [option...]
 
 Options:
+--prayer=PRAYER     retrieve an Hour of the Divine Office, or the Mass
+--action=ACTION     [$actions] [default: $action]
+--compare           retrieve "comparison" variant of PRAYER and ACTION
+--mobile            retrieve "mobile" variant of PRAYER and ACTION
 --version=VERSION   rubric version [no default]
---from=MM-DD-YYYY   start downloading for this date [default: today]
+--from=MM-DD-YYYY   start downloading for this date [default: $from]
 --to=MM-DD-YYYY     end downloading for this date [default: from-date]
---dir=DIR           put downlaods into directory DIR [default: current dir]
---url=BASE          base URL of site to download from
-                    [default: http://divinumofficium.com]
---entry=PATH        relative URL of entry point [default: horas/officium.pl]
---missa             uses --entry=missa/missa.pl and --hora=SanctaMissa
---cgi=PARAM=VALUE   query parameters passed directly as P1=V1&P2=V2...
-HORA                [$horae_list]
-VERSION             [$version_list]
+
+--query=QUERY       retrieve BASE/QUERY
+--cgi=PARAM=VALUE   query parameters passed directly as PARAM=VALUE&PARAM=VALUE...
+
+--dir=DIR           put downlaods into directory DIR [default: current directory]
+--url=BASE          base URL of site to download from [default: $base_url]
+
+--help              This.
+
+PRAYER              [$prayers]
+VERSION             [$versions]
 VERSION can be abbreviated as long as it's unambiguous.
-PARAM               [$params_list]
+QUERY               Any HTTP subquery, possibly including (CGI) query parameters
+PARAM               [$params]
                     or anything else (unchecked)
 VALUE               anything
+PARAM=VALUE may need to be escaped or quoted from the shell.
+Both PARAM and VALUE will be urlencoded for transmission.
 
-Download files are named by the hora and date.  Replay tests using divinum-replay.
+Download files are named by the hora and date and version; or by the entry if specified.
+To replay tests use divinum-replay.
+
 Note: the default URL is the live site. The default for divinum-replayh is the 
 environment variable DIVINUM_OFFICIUM_URL.  This allows -get to establish live results
 and -replay to test against a test site.
 USAGE
 
-my $hora;
 my $version_arg;
-my $version;
-my $entry = 'horas/officium.pl';
-my $from = "$m-$d-$y";
-my $to;
-my $dir;
-my $base_url;
-my $missa;
-my @cgi = ();
-
-$base_url = 'http://divinumofficium.com';
-
 GetOptions(
-    'hora=s' => \$hora,
+    'prayer=s' => \$prayer,
+    'action=s' => \$action,
+    'compare' => \$compare,
+    'mobile' => \$mobile,
     'version=s' => \$version_arg,
     'from=s' => \$from,
     'to=s' => \$to,
+
+    'query=s' => \$query,
+    'cgi=s' => \@cgi,
+
     'dir=s' => \$dir,
     'url=s' => \$base_url,
-    'entry=s' => \$entry,
-    'missa' => \$missa,
-    'cgi=s' => \@cgi
+
+    'help' => \$help
 ) or die $USAGE;
 
-die $USAGE unless $missa || ($hora && grep $hora eq $_, @horae);
-
-if ( $missa )
+if ( $help )
 {
-    $entry = 'missa/missa.pl';
-    $hora = 'SanctaMissa';
+    print STDOUT $USAGE;
+    exit 0;
 }
 
-die "A --version must be specified.\n" unless $version_arg;
+die "Specify one or --prayer or --query\n" unless ($prayer && !$query) || (!$prayer && $query);
 
-# Translate version_arg to version
-my $matches = 0;
-for my $v ( @versions )
+# If --prayer is specified, set things up aright.
+if ( $prayer )
 {
-    if ( index($v, $version_arg) >= 0 )
+    die "--pray=$prayer is unknown\n" unless $prayers{$prayer};
+    die "--action=$action is unknown\n" unless $actions{$action};
+    if ( $prayer =~ /Missa/i )
     {
-        $version = $v;
-        $matches = $matches + 1
+        if ( $action eq 'pray' )
+        {
+            $entry = $compare ? 'missa/Cmissa.pl': 'missa/missa.pl';
+        }
+        elsif ( $action eq 'kalendar' )
+        {
+            $entry = $compare ? 'missa/Ckalendar.pl': 'missa/kalendar.pl';
+        }
+        elsif ( $action eq 'edit' )
+        {
+            $entry = 'missa/medit.pl';
+        }
+        elsif ( $action eq 'popup' )
+        {
+            $entry = 'missa/mpopup.pl';
+        }
+    }
+    else
+    {
+        if ( $action eq 'pray' )
+        {
+            if ( $mobile )
+            {
+                $entry = 'horas/Pofficium.pl';
+            }
+            elsif ( $compare )
+            {
+                $entry = 'horas/Cofficium.pl'
+            }
+            else
+            {
+                $entry = 'horas/officium.pl';
+            }
+        }
+        elsif ( $action eq 'kalendar' )
+        {
+            $entry = $compare ? 'horas/Ckalendar.pl': 'horas/kalendar.pl';
+        }
+        elsif ( $action eq 'edit' )
+        {
+            $entry = 'horas/edit.pl';
+        }
+        elsif ( $action eq 'popup' )
+        {
+            $entry = 'horas/popup.pl';
+        }
+    }
+
+    if ( $action eq 'popup' )
+    { 
+        print STDERR "warning: version ignored\n" if $version_arg
+    }
+    else
+    {
+        die "Specify a --version.\n" unless $version_arg;
+
+        # Translate version_arg to version
+        my $matches = 0;
+        for my $v ( @versions )
+        {
+            if ( index($v, $version_arg) >= 0 )
+            {
+                $version = $v;
+                $matches = $matches + 1
+            }
+        }
+        die "error: --version=$version_arg is ambiguous\n" unless $matches < 2;
+        die "error: --version=$version_arg is invalid\n" unless $matches == 1;
     }
 }
-die "error: --version=$version_arg is ambiguous\n" unless $matches < 2;
-die "error: --version=$version_arg is invalid\n" unless $matches == 1;
+else
+{
+    $entry = $query;
+}
 
 die "Invalid date $from .\n" unless my ($y1,$m1,$d1) = Decode_Date_US($from);
 $to = $from unless $to;
@@ -117,22 +215,34 @@ while ( Date_to_Days($y1,$m1,$d1) <= Date_to_Days($y2,$m2,$d2) )
     my $date = "$m1-$d1-$y1";
     my @arglist = ();
 
-    push @arglist, "command=pray$hora";
-    push @arglist, "version=$version" if $version;
-    push @arglist, "date=$date";
+    if ( $prayer )
+    {
+        push @arglist, "command=pray$prayer";
+        push @arglist, "version=$version" if $version;
+        push @arglist, "date=$date";
+    }
     push @arglist, @cgi;
 
+    # Encode for URL transmission
+    for ( @arglist )
+    {
+        # Don't encode the equals-sign.
+        if ( /^([^=]*)=(.*)$/ )
+        {
+            $_ = url_encode($1) . '=' . url_encode($2)
+        }
+        else
+        {
+            $_ = url_encode($_)
+        }
+    }
     my $args = join('&', @arglist);
-    my $url = "$base_url/cgi-bin/$entry?$args";
-    $url =~ s/ /%20/g;
+    my $url = "$base_url/$entry?$args";
 
     print STDERR "$url\n";
     @result = `curl -s '$url'`;
 
-    my $file = "$hora-$date";
-    $file = "$file-$version" if $version;
-    $file =~ s/ /_/g;
-
+    my $file = resource_to_filename("$entry/$args");
     my $path = $dir ? "$dir/$file" : "$file";
     open OUT, ">$path" or die "Can't write $path\n";
     my @now = localtime;
@@ -142,4 +252,19 @@ while ( Date_to_Days($y1,$m1,$d1) <= Date_to_Days($y2,$m2,$d2) )
     close OUT;
 
     ($y1,$m1,$d1) = Add_Delta_Days($y1,$m1,$d1,1);
+}
+
+# Ad hoc conversion of resource identifier to mnemonic filename.
+sub resource_to_filename($)
+{
+    my $resource = shift;
+
+    # Remove common path, param name, punctuation, etc, and join the result using -.
+    $resource =~ s:^[^/]*/::;
+    $resource =~ s/\.pl//g;
+    $resource =~ s/command=(pray|setup|edit)//g;
+    $resource =~ s/\w+=//g;
+    $resource =~ s/%[0-9a-f][0-9a-f]/ /i;   # cheap urldecode : absolutely works
+
+    return join ('-', split(/\W+/, $resource));
 }
