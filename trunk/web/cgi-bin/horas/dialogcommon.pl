022f522dfff379add43956b39bba314a0def8874
igregord@gmail.com <igregord@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2012-01-15 14:43:43 +0000

    Fixed a potential infinite loop in whole-file inclusions.

--- trunk/web/cgi-bin/horas/dialogcommon.pl
+++ trunk/web/cgi-bin/horas/dialogcommon.pl
@@ -270,8 +270,9 @@ sub setupstring($$$%)
     /mx;
   
   # Do whole-file inclusions.
-  while (my ($incl_fname, undef, $incl_subst) = ($sections{'__preamble'} =~ /$inclusionregex/g))
+  while (my ($incl_fname, undef, $incl_subst) = ($sections{'__preamble'} =~ /$inclusionregex/gc))
   {
+    if ($fullpath =~ /$incl_fname\.txt/) { warn "Cyclic dependency in whole-file inclusion: $fullpath"; last; }
     my $incl_sections = setupstring($basedir, $lang, $incl_fname);
     $sections{$_} ||= ${$incl_sections}{$_} foreach (keys %{$incl_sections});
   }
