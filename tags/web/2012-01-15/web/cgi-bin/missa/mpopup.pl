4487d044a17547d65d869df1f1b10b45b37b4471
apmarcello3@gmail.com <apmarcello3@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2012-01-16 00:41:37 +0000

    2012-01-15 tag

---
+++ tags/web/2012-01-15/web/cgi-bin/missa/mpopup.pl
@@ -0,0 +1,132 @@
+#!/usr/bin/perl
+# vim: set encoding=utf-8 :
+use utf8;
+
+#áéíóöõúüûÁÉ
+# Name : Laszlo Kiss
+# Date : 01-20-08
+# Divine Office   popup
+
+package missa;
+#1;
+
+#use warnings;
+#use strict "refs";
+#use strict "subs";
+#use warnings FATAL=>qw(all);
+
+use POSIX;
+use FindBin qw($Bin);
+use CGI;
+use CGI::Cookie;;
+use CGI::Carp qw(fatalsToBrowser);
+use File::Basename;
+use LWP::Simple;
+use Time::Local;
+#use DateTime;
+
+$error = '';
+$debug = '';
+
+$q = new CGI;
+
+
+#*** collect standard items
+require "$Bin/../horas/dialogcommon.pl";
+require "$Bin/webdia.pl";
+require "$Bin/ordo.pl";
+#require "$Bin/ordocommon.pl";
+require "$Bin/../horas/do_io.pl";
+require "$Bin/../horas/horascommon.pl";
+require "$Bin/tfertable.pl";
+
+binmode(STDOUT, ':encoding(utf-8)');
+
+#*** get parameters
+getini('missa'); #files, colors
+
+%dialog = %{setupstring($datafolder, '', 'missa.dialog')};
+%setup = %{setupstring($datafolder, '', 'missa.setup')};
+
+eval($setup{'parameters'});
+eval($setup{'general'});  
+$popup = strictparam('popup');      
+$rubrics=strictparam('rubrics');
+$lang1 = strictparam('lang1');
+$lang2 = strictparam('lang2');
+$background = ($whitebground) ? "BGCOLOR=\"white\"" : "BACKGROUND=\"$htmlurl/horasbg.jpg\"";
+
+$only = ($lang1 && $lang1 =~ /$lang2/) ? 1 : 0;
+
+$title = "$popup";
+$title =~ s/[\$\&]//;
+
+#$tlang = ($lang1 !~ /Latin/) ? $lang1 : $lang2;
+#%translate = %{setupstring($datafolder, $tlang, "Ordo/Translate.txt")};
+
+ $text = gettext($popup, $lang1); 
+ $t = length($text);
+ $width = ($t > 300) ? 600 : 400;
+ $height = ($t > 300) ? $screenheight - 100 : 3*$screenheight/4;
+
+#*** generate HTML
+# prints the requested item from prayers hash as popup
+  htmlHead($title, 2);
+    print << "PrintTag";
+<BODY VLINK=$visitedlink LINK=$link BACKGROUND="$htmlurl/horasbg.jpg" onload="setsize()"> 
+<FORM ACTION="popup.pl" METHOD=post TARGET=_self>
+<H3 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title</I></B></FONT></H3>
+<P ALIGN=CENTER><BR>
+<TABLE BORDER=0 WIDTH=90% ALIGN=CENTER CELLPADDING=8 CELLSPACING=$border BGCOLOR='maroon'>
+<TR>
+PrintTag
+
+ $text =~ s/\_/ /g;
+ print "<TD $background WIDTH=50% VALIGN=TOP>" . setfont($blackfont,$text) . "</TD>\n";
+  $lang = $lang2;
+
+  if (!$only) {
+    $text = gettext($popup, $lang2);    
+	  $text =~ s/\_/ /g;
+	  print "<TD $background VALIGN=TOP>" . setfont($blackfont,$text) . "</TD></TR>\n";
+  }
+  print "</TABLE><BR>\n";
+
+print "<A HREF=# onclick=\"window.close()\">Close</A>";
+
+if ($error) {print "<P ALIGN=CENTER><FONT COLOR=red>$error</FONT><\P>\n";}
+if ($debug) {print "<P ALIGN=center><FONT COLOR=blue>$debug</FONT><\P>\n";}
+
+print "</FORM></BODY></HTML>";
+
+#*** javascript functions
+sub horasjs {
+ print << "PrintTag";
+
+<SCRIPT TYPE='text/JavaScript' LANGUAGE='JavaScript1.2'>
+
+function setsize() {
+  window.resizeTo($width, $height);
+}
+
+</SCRIPT>
+PrintTag
+} 
+
+sub gettext {
+  my $popup = shift;
+  my $lang = shift;
+  my $text = ''; 
+
+  my $fname = checkfile($lang, "Ordo/$popup.txt");
+  if ($text = join('\n', do_read($fname))) {
+  } else {$text = "$datafolder/$lang/Ordo/$popup.txt cannot open!";}
+  
+
+  if (!$rubrics) {$text =~ s/[#!].*?\n//g;} 
+
+  $text =~ s/#/!/g;
+  $text = resolve_refs($text);
+  return $text;
+}
+  	 
---
+++ tags/web/2012-01-15/web/www/horas/English/Martyrologium/m.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+#áéíóöõúüûÁÉ
+# Name : Laszlo Kiss
+# Date : 01-20-08
+# Divine Office
+
+package horas;
+#1;
+
+#use warnings;
+#use strict "refs";
+#use strict "subs";
+#use warnings FATAL=>qw(all);
+
+use POSIX;
+use FindBin qw($Bin);
+use File::Basename;
+
+
+opendir (DIR, ".") or die "datafolder cannot open!";
+ my @file = readdir(DIR);
+ close DIR;
+
+ foreach $file (@file) {
+   if ($file !~ /^([0-9][0-9])\.txt/i) {next;}
+   $month = $1;
+
+   open (INP, "$month.txt") or die "$month.txt cannot open for input!";
+   @a = <INP>;
+   close INP;
+   my $day = 0;
+   my $text = '';
+   foreach $line (@a) {
+     $line = chompd($line);
+     if (!$line || $line =~ /^\s*$/ || $line =~ /bissex/i || length($line) < 10) {next;}   
+     
+     if ($line =~ /^\s*([0-9]+)\s*?[a-z]+\s+(.*)\.\s*Lun/i) {  
+       my $d = $1;
+       my $name = $2;
+       if ($day) {
+         my $fname = sprintf("%02i-%02i", $month, $day);
+         open (OUT, ">$fname.txt") or die "$fname.txt cannot open for output";
+         print OUT $text;
+         close OUT;
+       }
+       $text = "$name\n_\n";
+       $day = $d;
+    }
+    if (!$day) {next;}
+    if ($line =~ /[a-z][a-z][a-z]\.\s+[A-Z]/) {next;}
+    $text .= "$line\n";
+  }
+  if ($day) {
+    my $fname = sprintf("%02i-%02i", $month, $day);
+    open (OUT, ">$fname.txt") or die "$fname.txt cannot open for output";
+    print OUT $text;
+    close OUT;
+  }
+}
+
+
+sub chompd {
+  my $a = shift;
+  chomp($a);
+  $a =~ s/\r//g;
+  return $a;
+}
