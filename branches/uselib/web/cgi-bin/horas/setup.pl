826b02981b55cdba24c26de11d1b12f30ab7fe8c
igregord@gmail.com <igregord@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2012-01-31 16:48:39 +0000

    [uselib] horas/setup.pl: Enabled stricture and tidied code.

--- branches/uselib/web/cgi-bin/horas/setup.pl
+++ branches/uselib/web/cgi-bin/horas/setup.pl
@@ -1,33 +1,43 @@
 #!/usr/bin/perl
+
+use strict;
+
 use utf8;
 # vim: set encoding=utf-8 :
 
 # Name : Laszlo Kiss
 # Date : 01-20-08
-# Divine Office  setuo
+# Divine Office setup
 
-$a = 1;
 
-#*** setup($command)
+#*** setuptable($command)
 # prints and handles $command item from horas.dialog hash (in www/horas folder)
-# using horas.setup hash also from ww/horas folder
-sub setuptable {
-  $command = shift;	
-  $title1 = $title;
+# using horas.setup hash also from www/horas folder
+sub setuptable
+{
+  our %setup;
+  our $input;
+
+  my $command = shift;
+
+  my $title1 = our $title;
   $title1 =~ s/setup/options/i;
+
   #*** set input table
-  eval("$setup{$command}");	
-  setup($command, getsetuppar($command));  
+  eval("$setup{$command}");
+  setup($command, getsetuppar($command));
 
   print << "PrintTag";
-<H1 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title1 </I></B></FONT></H1>
+<H1 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title1</I></B></FONT></H1>
 <TABLE WIDTH=75% BORDER=0 ALIGN=CENTER><TR><TD>
-$input;
+$input
 </TD></TR></TABLE>
 <P ALIGN=CENTER>
 <INPUT TYPE=SUBMIT NAME='button' VALUE=O.K.>
 </P>
 PrintTag
 
-$command = "change" . $command;
+  $command = 'change' . $command;
 }
+
+1;
