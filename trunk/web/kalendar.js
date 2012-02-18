507f70d1ade287a55516b89a919fd6ddc08bc2d5
aj@malton.name <aj@malton.name@c9c4d15e-31b0-aaee-4716-697641b7153a>
2012-02-18 14:50:23 +0000

    added time, date, link, formatting, and asynchrony to widget

--- trunk/web/kalendar.js
+++ trunk/web/kalendar.js
@@ -1,15 +1,44 @@
-function DivinumOfficiumKalendar(version)
+mensis = ['Januarii', 'Februarii', 'Martii', 'Aprilis', 'Maii', 'Iunii', 'Iulii', 'Augusti', 'Septembris', 'Octobris', 'Novembris', 'Decembris']
+
+function DivinumOfficiumKalendar(version, options)
 {
-    var kal = new XMLHttpRequest()
     var url = 'http://divinumofficium.com/cgi-bin/horas/officium.pl?command=kalendar'
-    if ( version )
+
+    options = options || {}
+    options.id = options.id || 'kalendar'
+    options.link = options.link || 'http://divinumofficium.com'
+
+    var now = new Date()
+    var now_date = "" + now.getDate() + "-" + (now.getMonth()+1) + "-" + now.getFullYear()
+    var display_date = "" + now.getDate() + "." + mensis[now.getMonth()] + ". a.D. " + now.getFullYear()
+    var now_hour = now.getHours()
+    var hour
+    var suffix = 'Laudes'
+    if ( 5 <= now_hour && now_hour < 18 )
     {
-        url += '&version=' + version
+        hour = 'in die'
     }
-    kal.open('GET', url, false)
+    else if ( 18 <= now_hour && now_hour < 24 )
+    {
+        hour = 'ad vesperas'
+        suffix = 'Vesperas'
+    }
+    else
+    {
+        hour = 'in nocte'
+    }
+    
+    document.write('<a href="'+options.link+'" style="text-decoration:none;font-style:italic;text-align:center">')
+    document.write(hour + " " + display_date + '<br>')
+    document.write('<span id="' + options.id + '"></a>')
+
+    var kal = new XMLHttpRequest()
+
+    var span = document.getElementById(options.id)
+    kal.onload = function(e) { span.innerHTML = e.target.responseText }
+
+    url = url+suffix
+    url = version ? (url+'&version='+version) : url
+    kal.open('GET', url, true)
     kal.send()
-    if ( kal.status == 200 )
-    {
-        document.write(kal.responseText)
-    }
 }
