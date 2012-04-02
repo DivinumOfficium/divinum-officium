4d60e0e16baeb9d2027facdc79f50b92bd448f38
apmarcello3@gmail.com <apmarcello3@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2012-04-02 19:01:23 +0000

    Tag for update

---
+++ tags/web/2012-04-02/cgi-bin/horas/popup.pl
@@ -0,0 +1,158 @@
+#!/usr/bin/perl
+use utf8;
+# vim: set encoding=utf-8 :
+
+# Name : Laszlo Kiss
+# Date : 01-20-08
+# Divine Office   popup
+
+package horas;
+#1;
+
+#use warnings;
+#use strict "refs";
+#use strict "subs";
+#use warnings FATAL=>qw(all);
+
+use POSIX;
+use FindBin qw($Bin);
+use CGI;
+use CGI::Cookie;;
+use CGI::Carp qw(fatalsToBrowser);
+use File::Basename;
+use LWP::Simple;
+use Time::Local;
+#use DateTime;
+use locale;
+
+$error = '';
+$debug = '';
+
+$q = new CGI;
+
+#***common variables arrays and hashes
+#filled  getweek()
+our @dayname; #0=Adv|{Nat|Epi|Quadp|Quad|Pass|Pen 1=winner title|2=other title
+
+#filled by getrank()
+our $winner; #the folder/filename for the winner of precedence
+our $commemoratio; #the folder/filename for the commemorated
+our $commune; #the folder/filename for the used commune
+our $communetype; #ex|vide
+our $rank; #the rank of the winner
+our $laudes; #1 or 2
+our $vespera; #1 | 3 index for ant, versum, oratio
+our $cvespera; #for commemoratio
+
+#filled by precedence()
+our %winner; #the hash of the winner 
+our %commemoratio; #the hash of the commemorated
+our %commune; # the hash of the commune
+our (%winner2, %commemoratio2, %commune2); #same for 2nd column
+our $rule; # $winner{Rank}
+our $communerule; # $commune{Rank}
+our $duplex; #1= simplex 2=semiduplex, 3=duplex 0=rest
+             #4 = duplex majus, 5=duplex II class 6=duplex I class 7=higher 
+our $sanctiname = 'Sancti';
+our $temporaname = 'Tempora';
+our $communename = 'Commune';
+
+#*** collect standard items
+require "$Bin/do_io.pl";
+require "$Bin/horascommon.pl";
+require "$Bin/dialogcommon.pl";
+require "$Bin/webdia.pl";
+require "$Bin/setup.pl";
+require "$Bin/horas.pl";
+require "$Bin/specials.pl";
+require "$Bin/specmatins.pl";
+require "$Bin/tfertable.pl";
+
+binmode(STDOUT,':encoding(utf-8)');
+
+#*** get parameters
+getini('horas'); #files, colors
+
+@dayname=split('=', $dayname);
+$daycolor = ($dayname =~ /feria/i) ? "black" : ($dayname =~ /Sabbato|Vigil/i) ? "blue" : "red";
+
+$command = $hora = strictparam('command');
+$setupsave = strictparam('setup');
+$setupsave =~ s/\~24/\"/g;
+
+%dialog = %{setupstring($datafolder, '', 'horas.dialog')};
+if (!$setupsave) {%setup = %{setupstring($datafolder, '', 'horas.setup')};}
+else {%setup = split(';;;', $setupsave);}
+
+eval($setup{'parameters'});
+eval($setup{'general'});  
+$popup = strictparam('popup');      
+$lang = strictparam('popuplang');
+$background = ($whitebground) ? "BGCOLOR=\"white\"" : "BACKGROUND=\"$htmlurl/horasbg.jpg\"";
+
+$only = ($lang1 && $lang1 =~ /^$lang2$/i) ? 1 : 0;
+precedence(); 
+
+$title = "$popup";
+$title =~ s/[\$\&]//;
+
+$expand = 'all';
+if ($popup =~ /\&/) {$popup =~ s /\s/\_/g;}   
+$tlang = ($lang1 !~ /Latin/) ? $lang1 : $lang2;
+%translate = %{setupstring($datafolder, $tlang, 'Psalterium/Translate.txt')};
+
+ print STDERR "\$popup = $popup\n";
+ $text = resolve_refs($popup, $lang1); 
+ $t = length($text);
+ #$text = resolve_refs($text, $lang1);   
+ $width = ($t > 300) ? 600 : 400;
+ $height = ($t > 300) ? $screenheight - 100 : 3*$screenheight/4;
+
+#*** generate HTML
+# prints the requested item from prayers hash as popup
+  htmlHead($title, 2);
+    print << "PrintTag";
+<BODY VLINK=$visitedlink LINK=$link BACKGROUND="$htmlurl/horasbg.jpg" 
+  onload="setsize()"> 
+<FORM>
+<H3 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title</I></B></FONT></H3>
+<P ALIGN=CENTER><BR>
+<TABLE BORDER=0 WIDTH=90% ALIGN=CENTER CELLPADDING=8 CELLSPACING=$border BGCOLOR='maroon'>
+<TR>
+PrintTag
+
+ $text =~ s/\_/ /g;
+ if ($lang1 =~ /Latin/i) {$text = jtoi($text);}
+ print "<TD $background WIDTH=50% VALIGN=TOP>" . setfont($blackfont,$text) . "</TD>\n";
+  $lang = $lang2;
+
+  if (!$only) {
+    $text = resolve_refs($popup, $lang2);    
+    #$text = resolve_refs($text, $lang2);   
+ 	  $text =~ s/\_/ /g;
+      if ($lang2 =~ /Latin/i) {$text = jtoi($text);}
+	  print "<TD $background VALIGN=TOP>" . setfont($blackfont,$text) . "</TD></TR>\n";
+  }
+  print "</TABLE><BR>\n";
+
+print "<A HREF=# onclick=\"window.close()\">Close</A>";
+
+if ($error) {print "<P ALIGN=CENTER><FONT COLOR=red>$error</FONT></P>\n";}
+if ($debug) {print "<P ALIGN=center><FONT COLOR=blue>$debug</FONT></P>\n";}
+
+print "</FORM></BODY></HTML>";
+
+#*** javascript functions
+sub horasjs {
+ print << "PrintTag";
+
+<SCRIPT TYPE='text/JavaScript' LANGUAGE='JavaScript1.2'>
+
+function setsize() {
+  window.resizeTo($width, $height);
+}
+
+</SCRIPT>
+PrintTag
+} 
+
