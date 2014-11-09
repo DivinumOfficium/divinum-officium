99faac9097f6c5495679db695c52b1ccca8164ab
laulicus <laulicus@gmail.com>
2014-11-09 13:47:36 +0000

    Magyar fixes

--- web/cgi-bin/horas/specials.pl
+++ web/cgi-bin/horas/specials.pl
@@ -369,6 +369,7 @@ sub specials {
 	if ($item =~ /(benedictus|magnificat)/i) {	 
 	  $comment = ($winner =~ /sancti/i) ? 3 : 2; 
 	  $prefix = ($lang =~ /English/i) ? 'Antiphon' : 'Antiphona';	   
+	  $prefix = ($lang =~ /Magyar/i) ? 'Antifóna' : 'Antiphona';
 	  setcomment($label, 'Source', $comment, $lang, $prefix);
 	  next;																 
 	}
