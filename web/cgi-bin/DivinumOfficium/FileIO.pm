ad0b18407db9e2ebb962f1fe8da17e11ac60dede
Gregor Dick <igregord@gmail.com>
2014-12-08 11:59:57 +0000

    Added explicit list of vernaculars.

---
+++ web/cgi-bin/DivinumOfficium/FileIO.pm
@@ -0,0 +1,24 @@
+package DivinumOfficium::FileIO;
+
+use strict;
+use warnings;
+
+# Temporary measure: if we got here, the parent directory is on the library
+# search path, so this will work.
+BEGIN
+{
+  package DivinumOfficium::FileIO;
+  require "horas/do_io.pl";
+}
+
+BEGIN
+{
+  require Exporter;
+
+  our $VERSION = 1.00;
+  our @ISA = qw(Exporter);
+  our @EXPORT_OK = qw(do_read do_write);
+}
+
+1;
+
