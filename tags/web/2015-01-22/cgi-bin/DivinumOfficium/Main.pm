bc50bbec48ddc6f9d69802baf839bdbe1ee64937
apmarcello3@gmail.com <apmarcello3@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2015-01-22 17:23:27 +0000

    Tag

---
+++ tags/web/2015-01-22/cgi-bin/DivinumOfficium/Main.pm
@@ -0,0 +1,32 @@
+package DivinumOfficium::Main;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use DivinumOfficium::FileIO qw(do_read);
+
+BEGIN
+{
+  require Exporter;
+
+  our $VERSION = 1.00;
+  our @ISA = qw(Exporter);
+  our @EXPORT_OK = qw(vernaculars);
+}
+
+
+#*** vernaculars($basedir)
+# Returns a list of available vernacular languages for the datafiles rooted at
+# $basedir.
+sub vernaculars
+{
+  my $basedir = shift;
+  my @lines = do_read("$basedir/Linguae.txt") or
+    croak q(Couldn't load language list.);
+  return @lines;
+}
+
+1;
+
