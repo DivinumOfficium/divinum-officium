6f28f7bc5dd8cbab090e6d8647fa9c93b0235bd5
apmarcello3@gmail.com <apmarcello3@gmail.com@c9c4d15e-31b0-aaee-4716-697641b7153a>
2015-02-02 19:32:06 +0000

    Fixes Favicon display in Safari and organizes information display in Officium

--- trunk/web/cgi-bin/horas/setup.pl
+++ trunk/web/cgi-bin/horas/setup.pl
@@ -4,13 +4,13 @@ use utf8;
 
 # Name : Laszlo Kiss
 # Date : 01-20-08
-# Divine Office  setup
+# Divine Office Setup
 
 $a = 1;
 
 #*** setup($command)
 # prints and handles $command item from horas.dialog hash (in www/horas folder)
-# using horas.setup hash also from ww/horas folder
+# using horas.setup hash also from www/horas folder
 sub setuptable {
   $command = shift;	
   $title1 = $title;
@@ -20,12 +20,12 @@ sub setuptable {
   setup($command, getsetuppar($command));  
 
   print << "PrintTag";
-<H1 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title1 </I></B></FONT></H1>
+<H1 ALIGN=CENTER><FONT COLOR=MAROON><B><I>$title1</I></B></FONT></H1>
 <TABLE WIDTH=75% BORDER=0 ALIGN=CENTER><TR><TD>
 $input;
 </TD></TR></TABLE>
 <P ALIGN=CENTER>
-<INPUT TYPE=SUBMIT NAME='button' VALUE=O.K.>
+<INPUT TYPE=SUBMIT NAME='button' VALUE=OK>
 </P>
 PrintTag
 
