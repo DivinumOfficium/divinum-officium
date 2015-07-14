DivinumOfficium/divinum-officium

commit dc0ac24c2a64c9dfdf092c3eade2bff58b20e7c6

Avoid $[`&'] in oratio().

web/cgi-bin/horas/specials.pl
@@ -1068,9 +1068,9 @@ sub oratio
 
     #* deletes added commemoratio
     if (($w =~ /!commemoratio/i && $hora !~ /(laudes|vespera)/i) ||
-    ($hora =~ /laudes/i && $w =~ /!commemoratio/i && $w =~ /(precedenti|sequenti)/i)) {
-    $w = $`; 
-    $w =~ s/\s*_$\s*//;  
+    ($hora =~ /laudes/i && $w =~ /!commemoratio/i && $w =~ /(.*?)(precedenti|sequenti)/is)) {
+      $w = $1;
+      $w =~ s/\s*_$\s*//;
     }
 
     if (!$w) {$w = 'Oratio missing';}
@@ -1096,8 +1096,8 @@ sub oratio
 
     if ($hora =~ /(Laudes|Vespera)/i && $winner{Rule} =~ /Sub unica conc/i) {
     if ($version !~ /1960/) {
-      if ($w =~ /\n\$Per .*?\s*$/) {$addconclusio = $&; $w = $`;}
-      if ($w =~ /\n\$Qui .*?\s*$/) {$addconclusio = $&; $w = $`;}
+      if ($w =~ /(.*?)(\n\$Per .*?\s*)$/s) {$addconclusio = $2; $w = $1;}
+      if ($w =~ /(.*?)(\n\$Qui .*?\s*)$/s) {$addconclusio = $2; $w = $1;}
     } else {$w =~ s/\$(Per|Qui) .*?\n//;}
     }
 

commit 47751fa593242d5bcdd4c364f9f3e16494353edd

Further fix to commemoration-removal in oratio().
Another regression from dc0ac24.

web/cgi-bin/horas/specials.pl
@@ -1098,8 +1098,8 @@ sub oratio
 
     if ($hora =~ /(Laudes|Vespera)/i && $winner{Rule} =~ /Sub unica conc/i) {
     if ($version !~ /1960/) {
-      if ($w =~ /(.*?)(\n\$Per .*?\s*)$/s) {$addconclusio = $2; $w = $1;}
-      if ($w =~ /(.*?)(\n\$Qui .*?\s*)$/s) {$addconclusio = $2; $w = $1;}
+      if ($w =~ /(.*?)(\n\$Per [^\n\r]*?\s*)$/s) {$addconclusio = $2; $w = $1;}
+      if ($w =~ /(.*?)(\n\$Qui [^\n\r]*?\s*)$/s) {$addconclusio = $2; $w = $1;}
     } else {$w =~ s/\$(Per|Qui) .*?\n//;}
     }
 