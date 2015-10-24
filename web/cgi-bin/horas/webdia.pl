7117c429d8f9cd5928e6a78b1daf8782a5f492bb
Gregor Dick <igregord@gmail.com>
2015-10-25 00:06:23 +0100

    Avoid U+271D (LATIN CROSS).
    It's an emoji, with the consequence that the user agent can render it
    more or less how it likes.  This seems to affect iOS 9.1 in particular.
    We now use other sorts of crosses instead.

--- web/cgi-bin/horas/webdia.pl
+++ web/cgi-bin/horas/webdia.pl
@@ -484,14 +484,14 @@ sub setcross
     }
     else
     {
-        # Cross type 3: Cross of Lorraine
+        # Cross type 3: CROSS OF LORRAINE
         my $csubst = "<span style='color:red; font-size:1.25em'>&#x2628;</span>";
         $line =~ s/\+\+\+/$csubst/g;
-        # Cross type 2: Greek Cross (Cross of Jerusalem)
+        # Cross type 2: MALTESE CROSS
         my $csubst = "<span style='color:red; font-size:1.25em'>&#x2720;</span>";
         $line =~ s/\+\+/$csubst/g;
-        # cross type 1: Latin Cross
-        my $csubst = "<span style='color:red; font-size:1.25em'>&#x271D;</span>";
+        # cross type 1: CROSS OF JERUSALEM
+        my $csubst = "<span style='color:red; font-size:1.25em'>&#x2629;</span>";
         $line =~ s/ \+ / $csubst /g;
     }
 
