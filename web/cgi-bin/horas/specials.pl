b7e460230f4f7e57f90f67367ca9752c2f83c861
mbab <marcin.babnis@gmail.com>
2016-02-05 09:27:09 +0100

    * Translation of 'Commemoratio'

--- web/cgi-bin/horas/specials.pl
+++ web/cgi-bin/horas/specials.pl
@@ -1415,7 +1415,8 @@ sub getcommemoratio {
   postprocess_vr($v, $lang);
 
   our %prayers;
-  my $w = "!Commemoratio $rank[0]\nAnt. $a\n_\n$v\n_\n$prayers{$lang}->{Oremus}\nv. $o\n"; 
+  $translate{$lang}{Commemoratio}=~s/\n//;
+  my $w = "!$translate{$lang}{Commemoratio} $rank[0]\nAnt. $a\n_\n$v\n_\n$prayers{$lang}->{Oremus}\nv. $o\n"; 
   return $w;
 }
 
