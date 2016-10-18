e70a44a3c54f145657763ad50810900c1937a1d9
Gregor Dick <igregord@gmail.com>
2016-10-18 22:55:21 +0100

    regress: Travis: Test different versions in different jobs.

--- regress/scripts/travis.sh
+++ regress/scripts/travis.sh
@@ -8,6 +8,7 @@ source "${basedir}/util.sh"
 parent_rev="$(echo ${TRAVIS_COMMIT_RANGE} |
 		perl -ne '/(.*)\.\.\./ && print $1')"
 test_rev="${TRAVIS_COMMIT}"
+short_version="${VERSION}"
 
 # Check validity of commits.
 git rev-parse --verify "${parent_rev}"
@@ -16,7 +17,8 @@ git rev-parse --verify "${test_rev}"
 mdy_date() { date "$@" +'%m-%d-%Y'; }
 
 # Test two consecutive years, starting from today.
-"${basedir}/generate-diff.sh" \
-  "${parent_rev}"             \
-  "${test_rev}"               \
-  <(echo "$(mdy_date):$(mdy_date -d '+ 2 years - 1 day')")
+"${basedir}/generate-diff.sh"                                  \
+  "${parent_rev}"                                              \
+  "${test_rev}"                                                \
+  <(echo "$(mdy_date):$(mdy_date -d '+ 2 years - 1 day')")     \
+  "${short_version}"
