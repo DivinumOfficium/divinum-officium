0b9a0398b192f2913aad3f40c0f12cc7b23a78ca
Albert P. Marcello, III <apmarcello3@gmail.com>
2018-11-10 13:56:06 -0500

    Accents

--- web/cgi-bin/horas/webdia.pl
+++ web/cgi-bin/horas/webdia.pl
@@ -485,10 +485,10 @@ sub setcross
     else
     {
         # Cross type 3: CROSS OF LORRAINE
-        my $csubst = "<span style='color:red; font-size:1.25em'>&#x2628;&#xFE0E;</span>";
+        my $csubst = "<span style='color:red; font-size:1.25em'>☨︎</span>";
         $line =~ s/\+\+\+/$csubst/g;
         # Cross type 2: MALTESE CROSS
-        my $csubst = "<span style='color:red; font-size:1.25em'>&#x2720;&#xFE0E;</span>";
+        my $csubst = "<span style='color:red; font-size:1.25em'>✠︎</span>";
         $line =~ s/\+\+/$csubst/g;
         # cross type 1: LATIN CROSS
         my $csubst = "<span style='color:red; font-size:1.25em'>+</span>";
