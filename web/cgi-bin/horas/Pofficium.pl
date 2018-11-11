3a65a25a0ec40697b51e1ccb9d11641223baaa75
Albert P. Marcello, III <apmarcello3@gmail.com>
2018-11-10 21:04:06 -0500

    Accents

--- web/cgi-bin/horas/Pofficium.pl
+++ web/cgi-bin/horas/Pofficium.pl
@@ -220,7 +220,7 @@ for ($i = 1; $i <= $completed; $i++) {$hcolor[$i] = 'maroon';}
   #generate HTML
   htmlHead($title, 2);
     print << "PrintTag";
-<BODY VLINK=$visitedlink LINK=$link BACKGROUND="$htmlurl/horasbg.jpg"> 
+<BODY VLINK=$visitedlink LINK=$link BACKGROUND="$htmlurl/horasbg.jpg" style="width:100%; height: 100%;"> 
 <FORM ACTION="$officium" METHOD=post TARGET=_self>
 PrintTag
 
