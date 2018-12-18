32df4bc7dd5403186593202865c134ff1a0185f6
Albert P. Marcello, III <apmarcello3@gmail.com>
2018-12-18 13:16:53 -0500

    Accents

--- web/cgi-bin/horas/Pofficium.pl
+++ web/cgi-bin/horas/Pofficium.pl
@@ -255,7 +255,7 @@ PrintTag
 } else {	#mainpage
   $pmode = 'main';
   $command = "";
-  $height = floor($screenheight * 4 / 12);
+  $height = floor($screenheight);
   $height2 = floor($height / 2);
 
   $headline = setheadline();
