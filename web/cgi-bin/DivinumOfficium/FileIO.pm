a3e62e196b23c3fdb0edd264c3f609a854344338
Martin Hasoň <martin.hason@gmail.com>
2019-11-28 13:20:49 +0100

    Fix code style with perltidy

--- web/cgi-bin/DivinumOfficium/FileIO.pm
+++ web/cgi-bin/DivinumOfficium/FileIO.pm
@@ -1,24 +1,19 @@
 package DivinumOfficium::FileIO;
-
 use strict;
 use warnings;
 
 # Temporary measure: if we got here, the parent directory is on the library
 # search path, so this will work.
-BEGIN
-{
+BEGIN {
+
   package DivinumOfficium::FileIO;
   require "horas/do_io.pl";
 }
 
-BEGIN
-{
+BEGIN {
   require Exporter;
-
   our $VERSION = 1.00;
   our @ISA = qw(Exporter);
   our @EXPORT_OK = qw(do_read do_write);
 }
-
 1;
-
