667e55e0c685f3f1efb2c37293b6f82b7dddb9b0
Albert P. Marcello, III <apmarcello3@gmail.com>
2020-02-05 13:50:11 -0500

    Colors for liturgical days

--- web/cgi-bin/horas/Cofficium.pl
+++ web/cgi-bin/horas/Cofficium.pl
@@ -192,11 +192,11 @@ our $octavam = '';    #to avoid duplication of commemorations
 # prepare title
 $daycolor =
     ($commune =~ /(C1[0-9])/) ? "blue"
-  : ($dayname[1] =~ /(Cathedra|oann|Vigilia Nativitatis)/i) ? "black"
+  : ($dayname[1] =~ /(Cathedra|oann|Vigilia Nativitatis|Cena)/i) ? "black"
   : ($dayname[1] =~ /(Pentecosten|Epiphaniam|post octavam)/i) ? "green"
-  : ($dayname[1] =~ /(Pentecostes|Martyr|Innocentium|Apostol)/i) ? "red"
-  : ($dayname[1] =~ /(Quattuor|Vigilia|Quadragesima|Septuagesim|Sexagesim|Quinquagesim|Ciner|Adventus)/i) ? "purple"
-  : ($dayname[1] =~ /Defunctorum/i) ? "grey"
+  : ($dayname[1] =~ /(Pentecostes|Martyr|Innocentium|Cruc|Apostol)/i) ? "red"
+  : ($dayname[1] =~ /(Defunctorum|Parasceve|Morte)/i) ? "grey"
+  : ($dayname[1] =~ /(Quattuor|Vigilia|Passionis|Quadragesima|Hebdomadæ Sanctæ|Septuagesim|Sexagesim|Quinquagesim|Ciner|Adventus)/i) ? "purple"
   : "black";
 build_comment_line();
 
