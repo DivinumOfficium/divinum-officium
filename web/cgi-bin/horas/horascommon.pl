85c08edead876a6481c69ce81f51152b35f3b399
Albert P. Marcello, III <apmarcello3@gmail.com>
2020-03-11 07:53:06 -0400

    Fixes ranking for pre-1960 Triduum

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -1587,7 +1587,7 @@ sub setheadline {
       $rankname = 'II. classis';
     } elsif ($version =~ /(1570|1910|Divino|1955)/ && $dayname[0] =~ /Pasc[07]/i && $dayofweek > 0) {
       $rankname = ($rank =~ 7) ? 'Duplex I. classis' : 'Semiduplex';
-    } elsif ($version =~ /(1570|1910|Divino|1955)/ && $dayname[0] =~ /Quad/i && $dayofweek > 0) {
+    } elsif ($version =~ /(1570|1910|Divino|1955)/ && $dayname[0] =~ /Quad/i && $dayname[0] !~ /Quad6-4|5|6/i && $dayofweek > 0) {
       $rankname = 'Simplex';
     } elsif ($version =~ /(1570|1910|Divino|1955)/ && $dayname[0] == /07-04/i && $dayofweek > 0) {
       $rankname = ($rank =~ 7) ? 'Duplex I. classis' : 'Semiduplex';
