b3b89c13b42bb4e43847b2bcc25ce39cd16538c1
Ben Yanke <ben@benyanke.com>
2020-05-05 00:19:54 -0500

    Add more redirects

--- web/cgi-bin/missa/kalendar.pl
+++ web/cgi-bin/missa/kalendar.pl
@@ -130,6 +130,12 @@ $title = "Ordo: $monthnames[$kmonth-1] $kyear";
 htmlHead($title, 2);
 print << "PrintTag";
 <BODY VLINK=$visitedlink LINK=$link BACKGROUND="$htmlurl/horasbg.jpg" >
+<script>
+// https redirect
+if (location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
+</script>
 <FORM ACTION="kalendar.pl" METHOD=post TARGET=_self>
 <INPUT TYPE=HIDDEN NAME=setup VALUE="$setupsave">
 <INPUT TYPE=HIDDEN NAME=date1 VALUE="$date1">
