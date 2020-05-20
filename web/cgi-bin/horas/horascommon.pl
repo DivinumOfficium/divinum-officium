e3dcbeaa10295d0b50888e55367b10b2be16c399
Albert P. Marcello, III <apmarcello3@gmail.com>
2020-05-20 08:58:10 -0400

    Accents

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -1116,6 +1116,10 @@ sub precedence {
   if ($version =~ /1960/ && $winner{Rule} =~ /No Sunday commemoratio/i && $dayofweek == 0) {
     $commemoratio = $commemoratio1 = $dayname[2] = '';
   }
+  
+  if ($version =~ /1960/ && $winner =~ /Pasc5-3/i && $srank < 5) {
+      $commemoratio = $commemoratio1 = $dayname[2]='';
+  }
 
   if ($commemoratio) {
     my $flag = ($commemoratio =~ /tempora/i && $tvesp == 1) ? 1 : 0;
