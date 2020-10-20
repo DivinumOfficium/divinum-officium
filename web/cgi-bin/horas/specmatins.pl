eaff957d84df743894dec9760d59370801261fc4
mbab <marcin.babnis@gmail.com>
2020-10-20 11:18:18 +0200

    MONASTIC code: fix Sundays Matins readings broken by d54f47c

--- web/cgi-bin/horas/specmatins.pl
+++ web/cgi-bin/horas/specmatins.pl
@@ -1162,7 +1162,7 @@ sub gettype1960 {
       $type = LT1960_OCTAVEII;
     } elsif ($rank < 2 || $dayname[1] =~ /(feria|vigilia|die)/i) {
       $type = LT1960_FERIAL;
-    } elsif ($dayname[1] =~ /dominica.*?semiduplex/i || $winner =~ /Pasc1\-0/i) {
+    } elsif ($version !~ /Monastic/i && ($dayname[1] =~ /dominica.*?semiduplex/i || $winner =~ /Pasc1\-0/i)) {
       $type = LT1960_SUNDAY;
     } elsif ($rank < 5) {
       $type = LT1960_SANCTORAL;
