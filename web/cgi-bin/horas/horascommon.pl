c168dd059117ca89c05b2e833fcd79a6cef8be39
Jefersson Nathan <malukenho.dev@gmail.com>
2020-12-17 02:33:20 +0100

    Add completorium in the dominican rite
    As the  Dominican rite  is very  close to  the Rubrics  of 1960,  we can
    easily  add it  as a  new way  of praying  the divinum  officium. It  is
    personally helping me as I usually pray the Dominican rite.
    
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -194,6 +194,7 @@ sub getrank {
     : ($version =~ /newcal/i) ? 'NC'
     : ($version =~ /1955/) ? 1955
     : ($version =~ /1960/) ? 1960
+    : ($version =~ /Praedicatorum/) ? 1960
     : 1960;
   our %kalendar = undef;
   our $kalendarkey = '';
