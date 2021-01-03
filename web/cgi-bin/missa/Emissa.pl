4f3e786a6a1e128506714c3c64150f3b5a1bc3d6
Albert P. Marcello, III <apmarcello3@gmail.com>
2021-01-03 12:55:59 -0500

    Update colors

--- web/cgi-bin/missa/Emissa.pl
+++ web/cgi-bin/missa/Emissa.pl
@@ -164,17 +164,13 @@ precedence();    #fills our hashes et variables
 
 # prepare title
 $daycolor =
-  ( $commune =~ /(C1[0-9])/ ) ? "blue"
-  : ( $dayname[1] =~
-      /(Cathedra|oann|Pasch|Confessor|Ascensio|Vigilia Nativitatis|Cena)/i )
-  ? "black"
-  : ( $dayname[1] =~ /(Pentecosten|Epiphaniam|post octavam)/i ) ? "green"
-  : ( $dayname[1] =~ /(Pentecostes|Evangel|Martyr|Innocentium|Cruc|Apostol)/i )
-  ? "red"
-  : ( $dayname[1] =~ /(Defunctorum|Parasceve|Morte)/i ) ? "grey"
-  : ( $dayname[1] =~
-/(Quattuor|Vigilia|Passionis|Quadragesima|Hebdomadæ Sanctæ|Septuagesim|Sexagesim|Quinquagesim|Ciner|Adventus)/i
-  ) ? "purple"
+    ($commune =~ /(C1[0-9])/) ? "blue"
+  : ($dayname[1] =~ /(Vigilia Pentecostes|Martyr)/i) ? "red"
+  : ($dayname[1] =~ /(Dedicatione|Cathedra|oann|Pasch|Confessor|Ascensio|Cena)/i) ? "black"
+  : ($dayname[1] =~ /(Vigilia|Quattuor|Passionis|Quadragesima|Hebdomadæ Sanctæ|Septuagesim|Sexagesim|Quinquagesim|Ciner|Adventus)/i) ? "purple"
+  : ($dayname[1] =~ /(Pentecosten|Epiphaniam|post octavam)/i) ? "green"
+  : ($dayname[1] =~ /(Pentecostes|Evangel|Innocentium|Sanguinis|Cruc|Apostol)/i) ? "red"
+  : ($dayname[1] =~ /(Defunctorum|Parasceve|Morte)/i) ? "grey"
   : "black";
 build_comment_line();
 
