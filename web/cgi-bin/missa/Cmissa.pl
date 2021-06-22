9f9b2b4ea6849995d893a069e3e034cedef6ddb7
APMarcello3 <apmarcello3@gmail.com>
2021-06-22 15:00:34 +0000

    Colors

--- web/cgi-bin/missa/Cmissa.pl
+++ web/cgi-bin/missa/Cmissa.pl
@@ -177,11 +177,11 @@ our $psalmnum2 = 0;
 $daycolor =
     ($commune =~ /(C1[0-9])/) ? "blue"
   : ($dayname[1] =~ /(Vigilia Pentecostes|Quattuor Temporum Pentecostes|Martyr)/i) ? "red"
-  : ($dayname[1] =~ /(Conversione|Dedicatione|Cathedra|oann|Pasch|Confessor|Ascensio|Vigilia Nativitatis|Cena)/i) ? "black"
+  : ($dayname[1] =~ /(Vigilia|Quattuor|Passionis|gesim|Hebdomadæ Sanctæ|Ciner|Adventus)/i) ? "purple"
+  : ($dayname[1] =~ /(Conversione|Dedicatione|Cathedra|oann|Pasch|Confessor|Ascensio|Cena)/i) ? "black"
   : ($dayname[1] =~ /(Pentecosten|Epiphaniam|post octavam)/i) ? "green"
-  : ($dayname[1] =~ /(Pentecostes|Evangel|Martyr|Innocentium|Cruc|Apostol)/i) ? "red"
+  : ($dayname[1] =~ /(Pentecostes|Evangel|Innocentium|Sanguinis|Cruc|Apostol)/i) ? "red"
   : ($dayname[1] =~ /(Defunctorum|Parasceve|Morte)/i) ? "grey"
-  : ($dayname[1] =~ /(Quattuor|Vigilia|Passionis|gesim|Hebdomadæ Sanctæ|Ciner|Adventus)/i) ? "purple"
   : "black";
 build_comment_line();
 
