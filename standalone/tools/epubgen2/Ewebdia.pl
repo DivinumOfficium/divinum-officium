501d17ef79d391edc4365853966fd04834c0a62e
Albert P. Marcello, III <apmarcello3@gmail.com>
2023-01-31 06:32:28 -0500

    Revert "fix headings in generated missale files"

--- standalone/tools/epubgen2/Ewebdia.pl
+++ standalone/tools/epubgen2/Ewebdia.pl
@@ -18,6 +18,19 @@ my $a = 4;
 our %fontOverrides=('red113bi'=>'<b>', 'black82'=>'<em>', 'red0i'=>'<span class="ri">', 'red63'=>'<span class="w">', 'red150bi'=>'<span class="a">');
 our %fontOverridesEnd=('red113bi'=>'</b>', 'black82'=>'</em>', 'red0i'=>'</span>', 'red63'=>'</span>', 'red150bi'=>'</span>');
 
+#*** htmlHead($title, $flag)
+# generated the standard head with $title
+sub htmlHead {
+  my $title = shift;
+  my $flag = shift;
+  if (!$title) {$title = ' ';}
+
+
+  print << "PrintTag";
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="la"><head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <link href="s.css" rel="stylesheet" type="text/css"/><title>$title</title></head>
+PrintTag
+}
+
 #*** setup($name, $script)
 # generates an input table
 # $name is the command name listed above the table
