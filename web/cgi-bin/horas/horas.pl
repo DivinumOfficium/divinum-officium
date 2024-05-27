1381364c4c86984c60eca3c82f73fe4a2ba83de2
FAJ-Munich <60485740+FAJ-Munich@users.noreply.github.com>
2024-05-27 22:44:50 +0200

    Single Ordinarium for Matutinum (except Epiphany day)

--- web/cgi-bin/horas/horas.pl
+++ web/cgi-bin/horas/horas.pl
@@ -1130,17 +1130,18 @@ sub getordinarium {
   if ($command =~ /Matutinum/i && $rule =~ /Special Matutinum Incipit/i) { $suffix .= "e"; }    # for Epiphanias
   if ($command =~ /Tertia|Sexta|Nona/i) { $command = 'Minor'; }    # identical for Terz/Sext/Non
 
-  if ($version =~ /(1955|1960|Newcal)/) {
-    $suffix .= "1960";
-  } elsif ($version =~ /Monastic/i) {
-    $suffix .= "M";
-  } elsif ($version =~ /Ordo Praedicatorum/i) {
-    $suffix .= "OP";
-  }
+	if ($command =~ /Prima/i) {
+		if ($version =~ /(1955|1960|Newcal)/) {
+			$suffix .= "1960";
+		} elsif ($version =~ /Monastic/i) {
+			$suffix .= "M";
+		} elsif ($version =~ /Ordo Praedicatorum/i) {
+			$suffix .= "OP";
+		}
+	}
 
   # don't loose time for non existent files
-  $suffix = '' if $command =~ /^Completorium|^Minor$|^Vespera$|^Laudes$/;
-  $lang = 'Latin' if $command !~ /^(?:Matutinum|Prima)$/;
+  $lang = 'Latin' if $command !~ /^(?:Prima)$/;
 
   my $fname = checkfile($lang, "Ordinarium/$command$suffix.txt");
 
