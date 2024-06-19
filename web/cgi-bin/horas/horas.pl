c3862e8245ed8a3e041ed2bebd4140586e4f557e
rl1234567890 <Roberto.Lionello@gmail.com>
2024-06-19 11:45:05 -0700

    Added provision for Italian date in  Martyrology

--- web/cgi-bin/horas/horas.pl
+++ web/cgi-bin/horas/horas.pl
@@ -1037,6 +1037,10 @@ sub gregor {
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December',
   );
+  my @months_it = (
+    'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+    'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre',
+  );
   $day = $leapday || $day;    # recover English date in Leap Years
   my $sfx1 =
       ($day > 3 && $day < 21) ? 'th'
@@ -1058,6 +1062,8 @@ sub gregor {
     return ("Roku Pańskiego $year");
   } elsif ($lang =~ /Francais/i) {
     return ("L'année du Seigneur $year, le $gday$sfx2 jour de la Lune");
+  } elsif ($lang =~ /Italiano/i) {
+    return ("Anno del Signore $year, $day $months_it[$month - 1], Luna $gday");
   } else {
     return ("$months[$month - 1] $day$sfx1 $year, the $gday$sfx2 day of the Moon,", $months[$month - 1]);
   }
@@ -1074,6 +1080,10 @@ sub luna {
   my @months = (
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December',
+  );
+  my @months_it = (
+    'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+    'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre',
   );
   my @ordinals = (
     'prima', 'secúnda', 'tértia', 'quarta',
@@ -1096,6 +1106,8 @@ sub luna {
 
   if ($lang =~ /Latin/i) {
     return ("Luna $ordinals[$dist-1]. Anno $year\n", ' ');
+  } elsif ($lang =~ /Italiano/i) {
+    return ("$day $months_it[$month - 1] $year, Luna $gday");
   } else {
     return ("$months[$month - 1] $day$sfx1 $year. The $dist$sfx2 day of the Moon.", $months[$month - 1]);
   }
