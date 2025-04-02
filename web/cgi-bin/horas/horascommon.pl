972e27e1be092cb2b45a4a1c17f7889653eaf32c
Augustinus-Altovadensis <84379704+Augustinus-Altovadensis@users.noreply.github.com>
2025-04-02 15:25:05 +0200

    Cist: Upd. rest of Temporale + Czech

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -746,7 +746,7 @@ sub concurrence {
 
     # before Divino Afflatu, the Sundays from Septuag to Judica and in Albis gave way at 2nd Vespers in concurrence to a Duplex
     $rank = $wrank[2] = 2.99;
-  } elsif ($dayname[0] =~ /Quad[0-5]|Quadp|Adv|Pasc1/ && $dayofweek == 0 && $version =~ /divino|altovadensis/i) {
+  } elsif ($dayname[0] =~ /Quad[0-5]|Quadp|Adv|Pasc1/ && $dayofweek == 0 && $version =~ /divino|cist/i) {
 
     # after Divino Afflatu, the Sundays from Septuag to Judica and in Albis gave way at 2nd Vespers in concurrence to a Duplex II. cl. (Cist: MM. maj.)
     $rank = $wrank[2] = $version =~ /divino/i ? 4.9 : 3.9;
