be17cd88e258d6a27f725b30bd0119c7694bf93c
FAJ-Munich <60485740+FAJ-Munich@users.noreply.github.com>
2025-07-09 20:48:10 +0200

    1888: Transfer of Immaculate Conception
    by the letter of the rubrics and against Ordo witness from Luxembourg from period of 1878 till 1895

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -368,6 +368,10 @@ sub occurrence {
     %saint = %{setupstring('Latin', $sname)};
     $srank = $saint{Rank};
     @srank = split(";;", $srank);
+  } elsif ($dayname[0] =~ /Adv|Quad/ && $srank[2] > 6) {
+    
+    # Making sure Duplex I. classis only outranks Major Sundays (after 1897) or Ferias when it is allowed by the rubrics
+    $srank[2] = 6.01;
   }
 
   if ($tname =~ /Epi1\-0/i && $srank[2] == 5.6) {
