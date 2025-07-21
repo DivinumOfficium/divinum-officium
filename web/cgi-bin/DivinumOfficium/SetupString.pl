9a1fe7d69afea8ed50921bae2e5b792ce421852b
FAJ-Munich <60485740+FAJ-Munich@users.noreply.github.com>
2025-07-21 20:49:01 +0200

    perltidy

--- web/cgi-bin/DivinumOfficium/SetupString.pl
+++ web/cgi-bin/DivinumOfficium/SetupString.pl
@@ -27,8 +27,7 @@ my %subjects = (
   votiva => sub { our $votive },
   officio => sub { $dayname[1]; },
   ad => sub { our $missa ? 'missam' : our $hora; },
-  mense => sub { our $month },
-  # mense is not perfect eg. 1 matches also 10 11 12
+  mense => sub { our $month },    # mense is not perfect eg. 1 matches also 10 11 12
 );
 my %predicates = (
   tridentina => sub { shift =~ /Trident/ },
