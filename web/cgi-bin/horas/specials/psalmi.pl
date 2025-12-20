615be6da7ec5e52c1656ece8dfe9bce4084e5e35
FAJ-Munich <60485740+FAJ-Munich@users.noreply.github.com>
2025-12-20 16:21:54 +0100

    Cantilenæ: Fix Expectetur (Monastic)

--- web/cgi-bin/horas/specials/psalmi.pl
+++ web/cgi-bin/horas/specials/psalmi.pl
@@ -410,7 +410,7 @@ sub psalmi_major {
 
       if ($version =~ /Monastic/) {
         $antiphones[2] = $expectetur;
-        $antiphones[3] = '';
+        $antiphones[3] = $lang =~ /gabc/ ? ';;;;4-alt-Astar' : '';
       } else {
         $antiphones[3] = $expectetur;
       }
