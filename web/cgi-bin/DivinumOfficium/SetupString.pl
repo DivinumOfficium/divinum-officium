69394ec47f4b141d30a7cc5c44aa4d5fcd701a62
Jiří Emmer <jirka.emmer@volny.cz>
2026-02-17 00:17:35 +0100

    CIST: Easter Vigil fixed

--- web/cgi-bin/DivinumOfficium/SetupString.pl
+++ web/cgi-bin/DivinumOfficium/SetupString.pl
@@ -198,7 +198,7 @@ sub get_tempus_id {
       : 'Septuagesimæ'
     : /^Quad(\d)/ && $1 < 5 ? 'Quadragesimæ'
     : /^Quad/ ? 'Passionis'
-    : /^Pasc0\-0/ && $dayofweek == 6 ? 'Vigilia Paschalis'
+    : /^Pasc0/ && $vesp_or_comp && $dayofweek == 6 ? 'Vigilia Paschalis'
     : /^Pasc0/ ? 'Octava Paschæ'
     : /^Pasc(\d)/ && ($1 < 5 || ($1 == 5 && ($dayofweek < 3 || (!$vesp_or_comp && $dayofweek == 3))))
     ? 'post Octavam Paschæ'
@@ -231,6 +231,7 @@ sub get_dayname_for_condition {
   return 'in Cœna Domini' if $winner =~ /Quad6\-4/;
   return 'in Parasceve' if $winner =~ /Quad6\-5/;
   return 'Sabbato Sancto' if $winner =~ /Quad6\-6/;
+  return 'Vigilia Paschalis' if $winner =~ /Pasc0\-0/ && $vesp_or_comp && $dayofweek == 6;
   return 'regis DNJC' if ($winner =~ /10\-DU/ || $commemoratio =~ /10\-DU/);
   return 'Omnium Defunctorum'
     if (
